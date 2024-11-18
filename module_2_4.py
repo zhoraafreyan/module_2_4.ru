662d9ece0a2827fd116510dc645db6426e80438f
zhoraafreyan <zondalift@gmail.com>
2024-11-18 18:21:54 +0600

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    for j in range(2, i):
+        if i % j == 0:
+            not_primes.append(i)
+            break
+    else:
+        primes.append(i)
+print(primes)
+print(not_primes)
